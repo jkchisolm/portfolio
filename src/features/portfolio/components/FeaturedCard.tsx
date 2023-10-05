4fa4aa2e0504c729e273d74e78cbea23379b33e3
Joshua Chisolm <jkchisolm22@gmail.com>
2023-10-05 16:35:13 -0700

    Add project cards

---
+++ src/features/portfolio/components/FeaturedCard.tsx
@@ -0,0 +1,56 @@
+import { FaGithub, FaLink } from "react-icons/fa6";
+
+type Props = {
+  title: string;
+  description: string;
+  imagePath: string;
+  backgroundColor?: string;
+  skills?: string[];
+  links: string[];
+  colorScheme: "light" | "dark";
+};
+
+export const FeaturedCard = (props: Props) => {
+  return (
+    <div className="grid grid-cols-1 grid-rows-5 gap-0 w-full h-full justify-center items-center rounded-lg">
+      <div className="row-span-2 bg-blue-500 h-full rounded-t-lg">
+        <div className="w-full h-full overflow-hidden">
+          <img
+            src={props.imagePath}
+            alt={props.title}
+            className="rounded-t-lg"
+          />
+        </div>
+      </div>
+      <div
+        className={`row-span-3 ${props.backgroundColor} rounded-b h-full flex 
+        flex-col justify-start items-start px-2 pb-2 pt-5 ${
+          props.colorScheme == "light" ? "text-white" : "text-black"
+        }`}
+      >
+        <div className="text-2xl font-bold">{props.title}</div>
+        <div>{props.description}</div>
+        {props.skills && (
+          <div className="flex flex-row justify-start items-center mt-2">
+            {props.skills.map((skill) => (
+              <div
+                className="bg-gray-100 text-gray-700 rounded-lg px-2 py-1 mr-2"
+                key={skill}
+              >
+                {skill}
+              </div>
+            ))}
+          </div>
+        )}
+        <div className="flex flex-row justify-start items-center justify-self-end mt-2">
+          <a href={props.links[0]} className="px-2">
+            <FaLink size={32} />
+          </a>
+          <a href={props.links[1]} className="px-2">
+            <FaGithub size={32} />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/features/portfolio/routes/Landing.tsx
+++ src/features/portfolio/routes/Landing.tsx
@@ -1,7 +1,11 @@
+import { FeaturedCard } from "../components";
+import MeImg from "../../../assets/me3.png";
+import RecreatifyImg from "../../../assets/Recreatify.png";
+
 export const LandingPage = () => {
   return (
-    <div className="flex flex-row justify-center items-center">
-      <div className="flex flex-col justify-center items-center w-full md:w-3/4 lg:w-1/2 mt-4">
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center w-full md:w-3/4 lg:w-1/2 px-3 mt-4">
         <div className="flex flex-col md:flex-row justify-start items-center w-full py-4">
           <h1 className="text-5xl font-bold">Hello World!</h1>
           <a href="/about" className="md:ml-7 text-xl text-green-500 font-bold">
@@ -20,6 +24,45 @@ export const LandingPage = () => {
           </span>{" "}
           I am a passionate developer who loves to constantly learn, grow, and
           build new projects.
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center w-full mt-8 px-3 md:px-14">
+        <h2 className="text-3xl font-bold self-start">Featured</h2>
+        <div
+          className="grid grid-cols-1 md:grid-cols-3 grid-rows-3 
+          md:grid-rows-1 gap-5 justify-center items-center w-full xs:min-h-[60rem] md:h-96 mt-4"
+        >
+          <div className="row-span-1 h-full rounded">
+            <FeaturedCard
+              title="Spotify.Me"
+              description="A recreation of Spotify.Me, a website that analyzes your Spotify listening habits 
+                                  and provides you with interesting statistics about your listening history."
+              imagePath={MeImg}
+              backgroundColor="bg-gradient-to-b from-cyan-400 to-teal-400 to-10%"
+              skills={["Front-end", "UI/UX", "React", "Next.js"]}
+              links={[
+                "https://meclone.jkchisolm.com",
+                "https://github.com/jkchisolm/new_spotify_me",
+              ]}
+              colorScheme="dark"
+            />
+          </div>
+          <div className="row-span-1 h-full">
+            <FeaturedCard
+              title="Recreatify"
+              description="A functioning recreation of the Spotify Web Player. 
+                          You can log in with your Spotify Premium account and 
+                          listen to your favorite tracks and playlists!"
+              imagePath={RecreatifyImg}
+              backgroundColor="bg-gradient-to-b from-zinc-900 to-green-400 to-5%"
+              skills={["Front-end", "UI/UX", "React", "Redux"]}
+              links={[
+                "https://recreatify.jkchisolm.com",
+                "https://github.com/jkchisolm/spotify-clone",
+              ]}
+              colorScheme="light"
+            />
+          </div>
         </div>
       </div>
     </div>
